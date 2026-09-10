38944c95b70617c0092da51ef8c6b979f4081048
Charles Lindsay <186342@cloud.rangitoto.school.nz>
2026-09-11 09:59:38 +1200

    leaderboard calculation starting

--- connect_4.py
+++ connect_4.py
@@ -1,5 +1,16 @@
 def connectfour():
+    first = ""
+    first_value = 0
+    second = ""
+    second_value = 0
+    third = ""
+    third_value = 0
+    fourth = ""
+    fourth_value = 0
+    fifth = ""
+    fifth_value = 0
     valid = False
+    mode = "name"
     print("Welcome to Connect 4!")
     while valid is False:
         print("\np - play  l - leaderboard  q - quit")
@@ -8,17 +19,46 @@ def connectfour():
             valid = True
         elif choice == "l" or choice == "leaderboard":
             try:
-                f = open("user_data.txt")
-                f.readline()
-                leaderboard = f.readline()
-                length_leaderboard = len(leaderboard)
-                if length_leaderboard == 0:
-                    print("No leaderboard data so far!")
-                else:
-                    print(leaderboard)
+                with open("game/connect_four_leaderboard.txt") as f:
+                    leaderboard = f.readline()
+                    length_leaderboard = len(leaderboard)
+                    if length_leaderboard == 0:
+                        print("No leaderboard data so far!")
+                    else:
+                        current = ""
+                        current_score = 0
+                        for i in range(length_leaderboard):
+                            if mode == "name":
+                                if leaderboard[i] != "$":
+                                    current += leaderboard[i]
+                                elif leaderboard[i] == "$":
+                                    mode = "wins"
+                            elif mode == "wins":
+                                if leaderboard[i] != "&":
+                                    current_score += leaderboard[i]
+                                elif leaderboard[i] == "&":
+                                    current_score = int(current_score)
+                                    if current_score > fifth_value:
+                                        current = fifth
+                                        current_score = fifth_value
+                                    if current_score > fourth_value:
+                                        fourth_value = fifth_value
+                                        fourth = fifth
+                                        current = fourth
+                                        current_score = fourth
+                                    if current_score > third_value:
+                                        fourth = fifth
+                                        fourth_value = fifth_value
+                                        third_value = fourth_value
+                                        third = fourth
+                                        current = third
+                                        current_value = third_value
+                                    
+
             except FileNotFoundError:
                 print("Sorry, there was an error loading the data!")
         elif choice == "q" or choice == "quit":
             valid = True
         else:
             print("Sorry, I don't know that one!")
+connectfour()

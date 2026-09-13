5fcde554298841b1305c0644cfdd91818eeec77f
Charles Lindsay <186342@cloud.rangitoto.school.nz>
2026-09-13 19:20:55 +1200

    leaderboard displaying works!!!

--- connect_4.py
+++ connect_4.py
@@ -19,14 +19,14 @@ def connectfour():
             valid = True
         elif choice == "l" or choice == "leaderboard":
             try:
-                with open("game/connect_four_leaderboard.txt") as f:
+                with open("connect_four_leaderboard.txt") as f:
                     leaderboard = f.readline()
                     length_leaderboard = len(leaderboard)
                     if length_leaderboard == 0:
                         print("No leaderboard data so far!")
                     else:
                         current = ""
-                        current_score = 0
+                        current_score = ""
                         for i in range(length_leaderboard):
                             if mode == "name":
                                 if leaderboard[i] != "$":
@@ -35,26 +35,53 @@ def connectfour():
                                     mode = "wins"
                             elif mode == "wins":
                                 if leaderboard[i] != "&":
-                                    current_score += leaderboard[i]
+                                    current_score += str(leaderboard[i])
                                 elif leaderboard[i] == "&":
+                                    mode = "name"
                                     current_score = int(current_score)
                                     if current_score > fifth_value:
-                                        current = fifth
-                                        current_score = fifth_value
-                                    if current_score > fourth_value:
-                                        fourth_value = fifth_value
-                                        fourth = fifth
-                                        current = fourth
-                                        current_score = fourth
-                                    if current_score > third_value:
-                                        fourth = fifth
-                                        fourth_value = fifth_value
-                                        third_value = fourth_value
-                                        third = fourth
-                                        current = third
-                                        current_value = third_value
-                                    
+                                        fifth = current
+                                        fifth_value = current_score
 
+                                    if current_score > fourth_value:
+                                        fifth_value = fourth_value
+                                        fifth = fourth
+                                        fourth = current
+                                        fourth = current_score
+
+                                    if current_score > third_value:
+                                        fifth = fourth
+                                        fifth_value = fourth_value
+                                        fourth_value = third_value
+                                        fourth = third
+                                        third = current
+                                        third_value = current_score
+
+                                    if current_score > second_value:
+                                        fifth = fourth
+                                        fifth_value = fourth_value
+                                        fourth_value = third_value
+                                        fourth = third
+                                        third = second
+                                        third_value = second_value
+                                        second = current
+                                        second_value = current_score
+
+                                    if current_score > first_value:
+                                        fifth = fourth
+                                        fifth_value = fourth_value
+                                        fourth_value = third_value
+                                        fourth = third
+                                        third = second
+                                        third_value = second_value
+                                        second = first
+                                        second_value = first_value
+                                        first = current
+                                        first_value = current_score
+
+                                    current_score = ""
+                                    current = ""
+                        print(first)        
             except FileNotFoundError:
                 print("Sorry, there was an error loading the data!")
         elif choice == "q" or choice == "quit":

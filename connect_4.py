b2cfd033ffcea6cb1dc349d508c706bbe95f833e
Charles Lindsay <186342@cloud.rangitoto.school.nz>
2026-09-13 19:27:40 +1200

    added some comments and added some formatting changes

--- connect_4.py
+++ connect_4.py
@@ -1,3 +1,4 @@
+"""Connect Four Function Definition File."""
 def connectfour():
     first = ""
     first_value = 0
@@ -10,19 +11,19 @@ def connectfour():
     fifth = ""
     fifth_value = 0
     valid = False
-    mode = "name"
+    mode = "name"  # Leaderboard uses to track whether currently checking for name or score
     print("Welcome to Connect 4!")
-    while valid is False:
+    while valid is False:  # Keeps going until the user has chosen a valid input
         print("\np - play  l - leaderboard  q - quit")
         choice = input("What would you like to do? ").lower()
         if choice == "p" or choice == "play":
             valid = True
         elif choice == "l" or choice == "leaderboard":
             try:
-                with open("connect_four_leaderboard.txt") as f:
+                with open("connect_four_leaderboard.txt") as f:  # (Attempts) opening the leaderboard data file
                     leaderboard = f.readline()
                     length_leaderboard = len(leaderboard)
-                    if length_leaderboard == 0:
+                    if length_leaderboard == 0:  # Checks if the leaderboard has no data yet
                         print("No leaderboard data so far!")
                     else:
                         current = ""
@@ -35,20 +36,18 @@ def connectfour():
                                     mode = "wins"
                             elif mode == "wins":
                                 if leaderboard[i] != "&":
-                                    current_score += str(leaderboard[i])
+                                    current_score += str(leaderboard[i])  # Adds the current score to a string in case it is more than single digit
                                 elif leaderboard[i] == "&":
                                     mode = "name"
                                     current_score = int(current_score)
                                     if current_score > fifth_value:
                                         fifth = current
                                         fifth_value = current_score
-
                                     if current_score > fourth_value:
                                         fifth_value = fourth_value
                                         fifth = fourth
                                         fourth = current
                                         fourth = current_score
-
                                     if current_score > third_value:
                                         fifth = fourth
                                         fifth_value = fourth_value
@@ -56,7 +55,6 @@ def connectfour():
                                         fourth = third
                                         third = current
                                         third_value = current_score
-
                                     if current_score > second_value:
                                         fifth = fourth
                                         fifth_value = fourth_value
@@ -66,8 +64,7 @@ def connectfour():
                                         third_value = second_value
                                         second = current
                                         second_value = current_score
-
-                                    if current_score > first_value:
+                                    if current_score > first_value:  # If the current checking score is higher than the current best, all the leaderboard moves down
                                         fifth = fourth
                                         fifth_value = fourth_value
                                         fourth_value = third_value
@@ -78,12 +75,11 @@ def connectfour():
                                         second_value = first_value
                                         first = current
                                         first_value = current_score
-
                                     current_score = ""
                                     current = ""
                         print(first)        
             except FileNotFoundError:
-                print("Sorry, there was an error loading the data!")
+                print("Sorry, there was an error loading the data!")  # Ensures the script doesn't crash when failing to access a file
         elif choice == "q" or choice == "quit":
             valid = True
         else:
